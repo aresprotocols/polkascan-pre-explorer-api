62a79f3b69817028e3b26bab572ae89f8cef365c
zzm <zzmwork8@gmail.com>
2022-02-25 11:13:00 +0800

    fix api

--- app/main.py
+++ app/main.py
@@ -64,7 +64,7 @@ app = falcon.API(middleware=[
 ])
 substrate = SubstrateInterface(url=settings.SUBSTRATE_RPC_URL, type_registry_preset=settings.TYPE_REGISTRY)
 # Application routes
-app.add_route('/chain', polkascan.ChainDataResource())
+app.add_route('/chain', polkascan.ChainDataResource(substrate))
 app.add_route('/oracle/symbols', oracle.SymbolListResource(substrate))
 app.add_route('/oracle/symbol/{symbol}', oracle.OracleDetailResource())
 app.add_route('/oracle/requests', oracle.OracleRequestListResource())
